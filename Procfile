784b4faa0dc4aa4feefa189be0274c4f4c7198fb
Aarukami <91074890+Aarukami@users.noreply.github.com>
2021-09-28 18:41:15 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,3 @@
+worker: python3 -m gojoAnimeBot
+web: python3 -m gojoAnimeBot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE gojo Robot
+:: Enables virtual env mode and then starts gojo
+env\scripts\activate.bat && py -m gojoAnimeBot
